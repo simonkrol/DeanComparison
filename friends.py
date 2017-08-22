d77332278be7fd08f4fbbecad6ec42acbb7fd50a
simonkrol <simonkrol11@gmail.com>
2017-08-21 23:38:32 -0400

    Add gitignore and config

--- friends.py
+++ friends.py
@@ -1,11 +1,14 @@
-
+import config
 import facebook
 
-ACCESS_TOKEN = "EAAKmexW29owBAAwxAIoeZBl6PVStngnCrdj0eqFF7byjG6O40LdBHt2ZAA86jizuGGT1OZBtQQOXh1mVuSQrSpCEin4wxeThd8v67QZCZAOAZCN0CAJ28obFg78MFp2cZAh5PXeaau61PgFUkkYVYryMEaAP4eslqS8KEo3b4M4856nirswiRLykI74NAZC1DtcZD"
-graph = facebook.GraphAPI(access_token=ACCESS_TOKEN, version='2.7')
-profile = graph.get_object("me")
-friends = graph.get_connections("me", "taggable_friends", limit="500")
+def get_fb():
+	ACCESS_TOKEN = config.ACCESS_TOKEN
+	graph = facebook.GraphAPI(access_token=ACCESS_TOKEN, version='2.7')
+	profile = graph.get_object("me")
+	friends = graph.get_connections("me", "taggable_friends", limit="10000")
 
-friend_list = [friend['name'] for friend in friends['data']]
-
-print(friend_list)
+	friend_list = [friend['name'] for friend in friends['data']]
+	return friend_list
+def get_deans():
+	print("Testing")
+print(get_fb())
